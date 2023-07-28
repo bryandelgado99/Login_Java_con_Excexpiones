ee9e72ba30d270f679fe094b0a257b189bd38324
Bryan Paul Delgado <115591939+bryandelgado99@users.noreply.github.com>
2023-07-28 12:44:25 -0500

    Initial commit

---
+++ src/Login.java
@@ -0,0 +1,71 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.sql.*;
+
+
+public class Login {
+
+    /*SQL DATABASE INTEGRATION*/
+    static final String DB_URL = "jdbc:mysql://localhost/epn_prueba";
+    static final String USER = "root";
+    static final String PASS = "root_bas3";
+    static final String QUERY = "SELECT ID_EST, CI_EST, Password_EST FROM estudiantes WHERE ID_EST = 20201213";
+    private JPanel rootPanel;
+    private JTextField userField;
+    private JTextField contraseña;
+    private JButton ingresarButton;
+
+
+    public Login() {
+
+        ingresarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try(Connection connection = DriverManager.getConnection(DB_URL, USER, PASS);
+                    Statement stmt = connection.createStatement();
+                    ResultSet rset = stmt.executeQuery(QUERY)){
+
+                        //Variables con datos de SQL y entradas de texto
+                        //Usuario
+                        String userinput = userField.getText();
+                        //Contraseña
+                        //String password = String.valueOf(rset.getInt("Password_EST"));
+                        //String pass = contraseña.getText();
+
+                        while(rset.next()){
+                            String username = String.valueOf(rset.getInt("ID_EST"));
+                            System.out.println(username);
+                            System.out.println(userinput);
+
+                            if(username == userinput){
+                                System.out.println("Hola");
+                            }else{
+                                System.out.println("Adios");
+                            }
+                        }
+
+                        /*//Validación de datos
+                        if(username == userinput){
+                            System.out.println("Hola");
+                        }else{
+                            System.out.println("Adios");
+                        }*/
+
+                }catch(SQLException n){
+                    throw new RuntimeException(n);
+                }
+            }
+        });
+    }
+
+        public static void main (String[]args){
+            JFrame frame = new JFrame("Login");
+            frame.setContentPane(new Login().rootPanel);
+            frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame.pack();
+            frame.setResizable(false);
+            frame.setSize(400, 300);
+            frame.setVisible(true);
+        }
+    }
